af0c218cb0daee61d9e54c06dfb999cf6f9761c3
naveen <naveenbollimpalli@gmail.com>
2015-05-11 06:20:38 +0530

    p issue

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module AdWyze
   class Application < Rails::Application
-    config.serve_static_assets = true
+
     unless Rails.env.test?
       log_level = String(ENV['LOG_LEVEL'] || "info").upcase
       config.logger = Logger.new(STDOUT)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.precompile += %w( angular.js )
-  config.assets.precompile += %w( bootstrap.js )
-  config.assets.precompile += %w( search_record.js )
-  config.assets.precompile += %w( visitor.js )
+
   config.assets.compress = true
   # Code is not reloaded between requests.
   config.cache_classes = true
